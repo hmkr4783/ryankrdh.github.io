acb97cbc3d6e6cade0e65b2e5c0cafc90c70ca7a
Ryankrdh <rhk4783@gmail.com>
2019-01-29 20:17:40 -0800

    practice

---
+++ .bash_history
@@ -0,0 +1,19 @@
+pwd
+cd projectws
+cd projects
+code .eslintrc.json
+cd ~
+ls -a
+code .bash_profile
+nano .profile
+ls -a
+ls -la
+code .bash_profile
+nano .profile
+nano .profile
+nano .profile
+pwd
+nano .profile
+cdwr
+cd
+cd ~
---
+++ app.js
@@ -0,0 +1,29 @@
+'use strict'
+
+// Design a guessing game about you that involves FIVE yes/no questions. 
+
+// The questions in the guessing game must require a mix of yes/no answers, 
+// and user input must accept either y/n or yes/no responses, with either .toUpperCase() 
+// or .toLowerCase() used to validate the user input and accommodate users entering all-caps Y/N or YES/NO answers, too.
+
+// Useful and descriptive console.log() messages in the JS are well written and correctly displaying to 
+// the browser console for each question of the guessing game.
+
+var dogs = prompt('How many dogs do I own');
+
+console.log('Number of dogs, ', dogs); 
+
+alert('Number of dogs, ' + dogs);
+
+//alert('Number of dogs, ' + dogs);
+
+var guitars = prompt('How many guitars do you have');
+
+if (guitars === '1' || guitars === '2'){
+    alert('Yes I have ' + guitars + ' guitars!')
+}
+
+
+use Strict;
+
+
