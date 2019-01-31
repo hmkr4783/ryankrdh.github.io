9288b52f1904eccac5c0e9b93955e8ce9e7c1de6
Ryankrdh <rhk4783@gmail.com>
2019-01-30 18:54:41 -0800

    second question added

--- project-directory/js/app.js
+++ project-directory/js/app.js
@@ -1,26 +1,43 @@
-'use strict'
+'use strict';
 
-var winterSport = prompt('My favorite winter sport is ice skating. yes/no')
-console.log('My favorite winter sport is ice skating. yes/no, ' + winterSport)
+//Introduction
+alert('Hello! My name is Ryan. Welcome to my guessing game!');
 
+//Question 1 winter sport
+var winterSport = prompt('Question 1: Is my favorite winter sport, ice skating? yes/no').toLowerCase();
+console.log('User answered ' + winterSport + ', to the question: \"Is my favorite winter sport, ice skating?\"');
 
-
-
-]var dogs = prompt('How many dogs do I own');
-
-console.log('Number of dogs, ', dogs); 
-
-alert('Number of dogs, ' + dogs);
-
-//alert('Number of dogs, ' + dogs);
-
-var guitars = prompt('How many guitars do you have');
-
-if (guitars === '1' || guitars === '2'){
-    alert('Yes I have ' + guitars + ' guitars!')
+if (winterSport === 'y'){
+  alert('Bzzzzzt!! Wrong.');
+  alert('But it\'s okay. On to the next question!');
+}else if (winterSport === 'yes'){
+  alert('Bzzzzzt!! Wrong.');
+  alert('But it\'s okay. On to the next question!');
+}else{
+  alert('Correct! My favorite winter sport is snowboarding not ice skating!');
+  alert('Good job. On to the next question!');
 }
 
+//Question 2 video games
+var videoGames = prompt('Question 2: Do I like to play video games? yes/no').toLowerCase();
+console.log('User answered ' + videoGames + ', to the question: \"Do I like to play video games?\"');
 
-use Strict;
+if (videoGames === 'y'){
+  alert('Correct! I love playing video games (When i have time...)');
+  alert('Nice. I mean who doesn\'t?');
+}else if (videoGames === 'yes'){
+  alert('Correct! I love playing video games (When i have time...)');
+  alert('Nice job. Who doesn\'t, right?');
+}else{
+  alert('Nice job. Who doesn\'t, right?');
+}
 
+//Question 3 My favorite pet.
 
+alert('Ready for the next question?')
+// var favoritePets = [bird, cat, dog]; // Array of pets
+// var arrayLength = favoritePets.length; // Items in array
+// var i;  // Counter
+// for (var i = 0; i < 4; i++) {
+//   var favoritePets = prompt('Question 3: Lis')
+// }
