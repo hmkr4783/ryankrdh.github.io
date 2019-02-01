f6992bd050129143fc09888e0044d95117f1b690
Ryankrdh <rhk4783@gmail.com>
2019-01-31 20:39:38 -0800

    3 questions completed with debugs

--- project-directory/js/app.js
+++ project-directory/js/app.js
@@ -3,33 +3,28 @@
 //Introduction
 alert('Hello! My name is Ryan. Welcome to my guessing game!');
 
-// //Question 1 winter sport.
-// var winterSport = prompt('Question 1: Is my favorite winter sport, ice skating? yes/no')
-//   .toLowerCase();
-// console.log('User answered ' + winterSport + ', to the question: "Is my favorite winter sport, ice skating?"');
+//Question 1 winter sport.
+var winterSport = prompt('Question 1: Is my favorite winter sport, ice skating? yes/no')
+  .toLowerCase();
+console.log('User answered ' + winterSport + ', to the question: "Is my favorite winter sport, ice skating?"');
+if (winterSport === 'y'){
+  alert('Bzzzzzt!! Wrong.');
+  alert('But it\'s okay. On to the next question!');
+}else if (winterSport === 'yes'){
+  alert('Bzzzzzt!! Wrong.');
+  alert('But it\'s okay. On to the next question!');
+}else{
+  alert('Correct! My favorite winter sport is snowboarding not ice skating!');
+  alert('Good job. On to the next question!');
+}
 
-// if (winterSport === 'y'){
-//   alert('Bzzzzzt!! Wrong.');
-//   alert('But it\'s okay. On to the next question!');
-// }else if (winterSport === 'yes'){
-//   alert('Bzzzzzt!! Wrong.');
-//   alert('But it\'s okay. On to the next question!');
-// }else{
-//   alert('Correct! My favorite winter sport is snowboarding not ice skating!');
-//   alert('Good job. On to the next question!');
-// }
-
-//Question 2 video games.
+// Question 2 video games.
 var videoGames = prompt('Question 2: Do I like to play video games? yes/no')
   .toLowerCase();
 console.log('User answered ' + videoGames + ', to the question: "Do I like to play video games?"');
-
-if (videoGames === 'y' ){
+if (videoGames === 'y' || videoGames === 'yes' || videoGames === 'yea' || videoGames === 'yeah'){
   alert('Correct! I love playing video games (When i have time...)');
   alert('Nice. I mean who doesn\'t?');
-}else if (videoGames === 'yes'){
-  alert('Correct! I love playing video games (When i have time...)');
-  alert('Nice job. Who doesn\'t, right?');
 }else{
   alert('Wrong!!');
 }
@@ -38,8 +33,7 @@ if (videoGames === 'y' ){
 alert('Ready for the next question?');
 var favoritePets = prompt('What\'s my favorite pet?')
   .toLowerCase();
-while(
-  if (favoritePets !== 'dog'){
+while(favoritePets !== 'dog' && favoritePets !== 'dogs'){
   console.log('User answered ' + favoritePets + ', to the question: "What\'s my favorite pet?"');
   favoritePets = prompt('Nope! Try again!');
 }
@@ -47,36 +41,18 @@ alert('Correct! I love dogs!');
 console.log('User answered ' + favoritePets + ', to the question: "What\'s my favorite pet?"');
 
 
-// //Question 3 My favorite pet.
-// alert('Ready for the next question?');
-// var favoritePets = prompt('What\'s my favorite pet?')
-//   .toLowerCase();
-// while(favoritePets !== 'dog' || 'dogs'){
-//   console.log('User answered ' + favoritePets + ', to the question: "What\'s my favorite pet?"');
-//   favoritePets = prompt('Nope! Try again!');
-// }
-// alert('Correct! I love dogs!');
-// console.log('User answered ' + favoritePets + ', to the question: "What\'s my favorite pet?"');
+//Question 4 My coffee preference.
+var coffeePreference = prompt('What is my favorite coffee?')
+  .toLowerCase();
+while(coffeePreference !== 'latte' && coffeePreference !== 'cafe latte' && coffeePreference !==
+'espresso' && coffeePreference !== 'espresso shot') {
+  console.log('User answered ' + coffeePreference + ', to the question: "What is my favorite coffee?"');
+  coffeePreference = prompt('Try again!!');
+}
+console.log('User answered ' + coffeePreference + ', to the question: "What is my favorite coffee?"');
+do {
+  var teaPreference = prompt('Great job! How about tea? black tea or green tea?')
+    .toLowerCase();
+  console.log('User answered ' + teaPreference + ', to the question: "What\'s my favorite tea?"');
+} while(teaPreference !== 'black tea' && teaPreference !== 'earl grey' && teaPreference !== 'black');
 
-
-// //Question 4 My coffee preference.
-// var coffeePreference = prompt('How do I like my coffee? creme, creme and sugar, sugar, or black?')
-//   .toLowerCase();
-// while(coffeePreference !== 'black') {
-//   console.log('User answered ' + coffeePreference + ', to the question: "What\'s my coffee preference?"');
-//   coffeePreference = prompt('Try again!!');
-// }
-// do {
-//   var teaPreference = prompt('Great job! How about tea? black tea or green tea?')
-//     .toLowerCase();
-// } while(teaPreference !== 'black tea');
-// console.log('User answered ' + teaPreference + ', to the question: "What\'s my tea preference');
-
-
-
-// for(var i = 0; i < favoritePets.length; i++) {
-// var arrayLength = favoritePets.length; // Items in array
-// var i;  // Counter
-// for (var i = 0; i < 4; i++) {
-//   var favoritePets = prompt('Question 3: Lis')
-// }
